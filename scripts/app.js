0712f6f0abfb5c4a6b979f6eb12b0d6b64d4cf30
DanCNo <dan.noh@gmail.com>
2019-06-25 17:06:19 -0400

    add dots

--- scripts/app.js
+++ scripts/app.js
@@ -14,8 +14,17 @@ function showSlides(idx){
 
   var slides = document.getElementsByClassName("image");
 
+  var dots = document.getElementsByClassName("dot");
+
   slideIndex = Math.abs(idx) % 4;
 
   slides[0].style = `background-image: url(./images/hero-image-${slideIndex}.jpg)`;
 
+  for (let i = 0; i < dots.length; i++) {
+    dots[i].className = dots[i].className.replace(" active", "");
+  }
+
+  dots[slideIndex].className += " active";
+
+  
 }
